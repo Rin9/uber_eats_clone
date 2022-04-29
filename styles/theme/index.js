cc9c678783c0e038bcb3ab5bb59bb383d46336e0
Rin9 <formygame38@gmail.com>
2022-04-29 19:47:04 -0400

    update project

--- styles/theme/index.js
+++ styles/theme/index.js
@@ -6,6 +6,32 @@ import { Text } from "./components/text";
 import { Heading } from "@chakra-ui/react";
 
 const overrides = {
+  colors: {
+    lime: {
+      50: "#f2ffde",
+      100: "#defcb2",
+      200: "#caf884",
+      300: "#b5f554",
+      400: "#a1f226",
+      500: "#88d90d",
+      600: "#69a905",
+      700: "#4a7801",
+      800: "#2b4800",
+      900: "#0b1900",
+    },
+    black: {
+      50: "#0d0d0d",
+      100: "#0d0d0d",
+      200: "#0d0d0d",
+      300: "#0d0d0d",
+      400: "#0d0d0d",
+      500: "#0d0d0d",
+      600: "#0d0d0d",
+      700: "#0d0d0d",
+      800: "#0d0d0d",
+      900: "#0d0d0d",
+    },
+  },
   styles: {
     global: {
       "html, body": {
